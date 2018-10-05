58154d9343c8e92608368284143d1292348889e5
JDSM <jesusdmalave@gmail.com>
2018-10-04 23:58:54 -0500

    proyecto administrativo dev categoria

---
+++ app/Categoria.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    //protected $table = 'categorias'; // no se implementa  este metodo ya que la tabla se llama igual que el modelo
+    //protected $primaryKey = 'id'; // no se implementa ya que eloquent asume que toda tabla tiene un id como primaryKey
+    protected $fillable = ['nombre','descripcion','condicion'];
+    
+}
