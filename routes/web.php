8ae9e94ea7ce3cdbebb23efcb465e791e09fd42a
JDSM <jesusdmalave@gmail.com>
2019-01-26 17:42:36 -0500

    create logout, migrate table ingreso and detalle_ingreso, modelos y controllers 75

--- routes/web.php
+++ routes/web.php
@@ -10,42 +10,82 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/','Auth\LoginController@showLoginForm');
+    Route::post('/login', 'Auth\LoginController@login')->name('login');
+});
+Route::group(['middleware' => 'auth'], function () {
+    Route::post('/logout','Auth\LoginController@logout')->name('logout');
+    Route::get('/main', function () {
+        return view('contenido/contenido');
+    })->name('main');
+    Route::group(['middleware' => 'Almacenista'], function () {
+        //Categoria
+        Route::get('/categoria','CategoriaController@index');
+        Route::post('/categoria/registrar','CategoriaController@store');
+        Route::put('/categoria/actualizar','CategoriaController@update');
+        Route::put('/categoria/desactivar','CategoriaController@desactivar');
+        Route::put('/categoria/activar','CategoriaController@activar');
+        Route::get('/categoria/selectCategoria','CategoriaController@selectCategoria');    
+        //Articulo
+        Route::get('/articulo','ArticuloController@index');
+        Route::post('/articulo/registrar','ArticuloController@store');
+        Route::put('/articulo/actualizar','ArticuloController@update');
+        Route::put('/articulo/desactivar','ArticuloController@desactivar');
+        Route::put('/articulo/activar','ArticuloController@activar');
+        //Proveedor
+        Route::get('/proveedor','ProveedorController@index');
+        Route::post('/proveedor/registrar','ProveedorController@store');
+        Route::put('/proveedor/actualizar','ProveedorController@desactivar');
+        //ingreso
+        Route::get('/ingreso','IngresoController@index');
+        Route::post('/ingreso/registrar','IngresoController@store');
+        Route::put('/ingreso/actualizar','IngresoController@update');
+    });
+    
+    Route::group(['middleware' => 'Vendedor'], function () {
+        //Cliente
+        Route::get('/cliente','ClienteController@index');
+        Route::post('/cliente/registrar','ClienteController@store');
+        Route::put('/cliente/actualizar','ClienteController@update');
+    });
+    
+    Route::group(['middleware' => ['Administrador']], function () {
+        //Categoria
+        Route::get('/categoria','CategoriaController@index');
+        Route::post('/categoria/registrar','CategoriaController@store');
+        Route::put('/categoria/actualizar','CategoriaController@update');
+        Route::put('/categoria/desactivar','CategoriaController@desactivar');
+        Route::put('/categoria/activar','CategoriaController@activar');
+        Route::get('/categoria/selectCategoria','CategoriaController@selectCategoria');    
+        //Articulo
+        Route::get('/articulo','ArticuloController@index');
+        Route::post('/articulo/registrar','ArticuloController@store');
+        Route::put('/articulo/actualizar','ArticuloController@update');
+        Route::put('/articulo/desactivar','ArticuloController@desactivar');
+        Route::put('/articulo/activar','ArticuloController@activar');
+        //Proveedor
+        Route::get('/proveedor','ProveedorController@index');
+        Route::post('/proveedor/registrar','ProveedorController@store');
+        Route::put('/proveedor/actualizar','ProveedorController@update');
+        //Cliente
+        Route::get('/cliente','ClienteController@index');
+        Route::post('/cliente/registrar','ClienteController@store');
+        Route::put('/cliente/actualizar','ClienteController@update');
+        //Rol
+        Route::get('/rol','RolController@index');
+        Route::get('/rol/selectRol','RolController@selectRol');
+        //Usuario
+        Route::get('/user','UserController@index');
+        Route::post('/user/registrar','UserController@store');
+        Route::put('/user/actualizar','UserController@update');
+        Route::put('/user/desactivar','UserController@desactivar');
+        Route::put('/user/activar','UserController@activar');
+        //ingreso
+        Route::get('/ingreso','IngresoController@index');
+        Route::post('/ingreso/registrar','IngresoController@store');
+        Route::put('/ingreso/actualizar','IngresoController@update');
+    });
+});
 
-Route::get('/main', function () {
-    return view('contenido/contenido');
-})->name('main');
-//Categoria
-Route::get('/categoria','CategoriaController@index');
-Route::post('/categoria/registrar','CategoriaController@store');
-Route::put('/categoria/actualizar','CategoriaController@update');
-Route::put('/categoria/desactivar','CategoriaController@desactivar');
-Route::put('/categoria/activar','CategoriaController@activar');
-Route::get('/categoria/selectCategoria','CategoriaController@selectCategoria');
-//Articulo
-Route::get('/articulo','ArticuloController@index');
-Route::post('/articulo/registrar','ArticuloController@store');
-Route::put('/articulo/actualizar','ArticuloController@update');
-Route::put('/articulo/desactivar','ArticuloController@desactivar');
-Route::put('/articulo/activar','ArticuloController@activar');
-//Cliente
-Route::get('/cliente','ClienteController@index');
-Route::post('/cliente/registrar','ClienteController@store');
-Route::put('/cliente/actualizar','ClienteController@update');
-//Proveedor
-Route::get('/proveedor','ProveedorController@index');
-Route::post('/proveedor/registrar','ProveedorController@store');
-Route::put('/proveedor/actualizar','ProveedorController@update');
-//Rol
-Route::get('/rol','RolController@index');
-Route::get('/rol/selectRol','RolController@selectRol');
-//Usuario
-Route::get('/user','UserController@index');
-Route::post('/user/registrar','UserController@store');
-Route::put('/user/actualizar','UserController@update');
-Route::put('/user/desactivar','UserController@desactivar');
-Route::put('/user/activar','UserController@activar');
-
-Route::get('/','Auth\LoginController@showLoginForm');
-Route::post('/login', 'Auth\LoginController@login')->name('login');
-
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
